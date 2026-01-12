c4169c84000d8f9704057b297216e6221046a975
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-01-12 22:48:18 +0530

    basic Oops

---
+++ OOPs/script.js
@@ -0,0 +1,21 @@
+let obj = {
+  name: "priyanshu",
+  age: 20,
+  class: 5,
+};
+
+console.log(obj);
+
+class Animal {
+  constructor() {
+    console.log("Object is created..");
+  }
+  eat() {
+    console.log("animla eat");
+  }
+  live() {
+    console.log("animla live");
+  }
+}
+let  a = new Animal();
+console.log(a)
