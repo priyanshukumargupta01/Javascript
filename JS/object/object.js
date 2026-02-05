faf3324312980a4c7e6924c0b8ece6ab10b6a56c
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-02-06 00:10:52 +0530

    more

---
+++ JS/object/object.js
@@ -0,0 +1,42 @@
+const user = { 
+    Name : "happy",
+    age : "20",
+    education : 12,
+    hobbies : "playing",
+    lastname:"kumar"
+}
+
+const user2 = { 
+    Name : "happy",
+    age : "20",
+    education : 12,
+    hobbies : "playing",
+    lastname:"kumar"
+}
+
+
+const s1 =""
+const s2 =""
+
+console.log(user===user2)
+console.log(s1===s2)
+//both object has same memory address but in case of string , s1 and s2 booth has same memory address
+
+
+
+console.log(user.lastname)
+
+user2.age="3";
+console.log(user2)
+
+const user3 = { 
+    Name : "happy",
+    lastname:"kumar",
+    age : "20",
+    education :{
+        Qualiification : 12,
+        hobbies : "playing"
+}}
+
+
+console.log(user3)
--- JS/object/tempCodeRunnerFile.js
+++ JS/object/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-const obj ={
-    name : "pk",
-    marks : 33,
-    grade : "B"
-}
-
-for (const element of obj) {
-    console.log(element)
-    
-}
+playing
