b7aea160c15dd804cfd316b8a2c7166341cfa125
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-02-08 01:26:54 +0530

    more

--- React/ELement/script.js
+++ React/ELement/script.js
@@ -1,1 +1,15 @@
-const h2 = React.createElement('h1')
+/*  directly in js using DOM
+let h2 = document.createElement("h1")
+h2.innerHTML= "hihihihihiii"
+document.querySelector("#root").append(h2)
+console.dir(h2)
+*/
+
+
+
+const h2 = React.createElement('h2' , {className :  "subHeading"},"Hello react")   
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+
+root.render(h2)
