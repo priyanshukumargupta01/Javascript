a622dd957e6107ac64ce8f76d2d7853677066daf
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-12 23:09:06 +0530

    Basic importand export

--- React/ELement/script.js
+++ React/ELement/script.js
@@ -5,11 +5,21 @@ document.querySelector("#root").append(h2)
 console.dir(h2)
 */
 
+const h2 = React.createElement(
+  'h2' , 
+  {
+    className :  "subHeading",id :"container"},
+    [
+    React.createElement("section",{},[
+      React.createElement( 
+      "p",
+      {},
+       "Hello react"
+      )
+    ])
+  ]
+   )
 
-
-const h2 = React.createElement('h2' , {className :  "subHeading"},"Hello react")   
-
-const root = ReactDOM.createRoot(document.querySelector("#root"))
-
-
-root.render(h2)
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(h2);
+ 
