898f690efb1bc147e2a06428215b01c2333cba3a
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-13 22:08:59 +0530

    import and export

--- React/ImportExport/app.js
+++ React/ImportExport/app.js
@@ -1,4 +1,7 @@
- var a = 20;
 
+ const arr =[7,23,3]
+ 
+export const user ="priyanshu" //named export
+export  var a = 20;
+export default arr;  //  this for when there is one value to import
 
-export default a;
--- React/ImportExport/script.js
+++ React/ImportExport/script.js
@@ -1,5 +1,7 @@
-import a from "./app.js"
-import user from "./app.js"
+import {a} from "./app.js"
+import {user} from "./app.js"
+import arr from "./app.js"
 
 console.log(a);
 console.log(user)
+console.log(arr);
