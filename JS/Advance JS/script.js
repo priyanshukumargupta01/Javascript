9a7a8ef89020943fab5acf959142f67dea55b0a4
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-07 23:48:46 +0530

    More

--- JS/Advance JS/script.js
+++ JS/Advance JS/script.js
@@ -1,17 +1,14 @@
-        async function sleep() {
-            return new Promise((resolve, reject) =>{
-                setTimeout(() => {
-                    resolve(45)
-                }, 3000);
-  
-        })
-          }
- 
-          (async function main() {
-            
-          let a = await sleep()
-          console.log(a)
-          let b = await sleep()
-                    console.log(b)
+async function sleep() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(45);
+    }, 3000);
+  });
+}
 
-  })() 
+(async function main() {
+  let a = await sleep();
+  console.log(a);
+  let b = await sleep();
+  console.log(b);
+})();
