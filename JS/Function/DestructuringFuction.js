70229840f78e325bfaee261a800cfcfaebfe6d28
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-16 23:46:13 +0530

    more

--- JS/Function/DestructuringFuction.js
+++ JS/Function/DestructuringFuction.js
@@ -1,10 +1,32 @@
-function userInfo({ name, age }) {
-  console.log(`Name: ${name}, Age: ${age}`);
+// function userInfo({ name, age }) {
+//   console.log(`Name: ${name}, Age: ${age}`);
+// }
+
+// const user = {
+//   name: "Priyanshu",
+//   age: 20
+// };
+
+// userInfo(user);
+
+ 
+
+
+function getUsers({ data: { users } }) {
+  console.log(users);
 }
 
-const user = {
-  name: "Priyanshu",
-  age: 20
+const response = {
+  data: {
+    users: ["A", "B", "C"]
+  }
 };
 
-userInfo(user);
+getUsers(response);
+
+
+function printNumbers([a, b, c]) {
+  console.log(a, b, c);
+}
+
+printNumbers([10, 20, 30]);
