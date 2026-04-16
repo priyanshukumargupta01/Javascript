227e5bd478c6d60638c24d5be20e5883023f45bc
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-16 23:43:11 +0530

    more

--- JS/Destructuring/script.js
+++ JS/Destructuring/script.js
@@ -1,43 +1,56 @@
-let arr =[3, 5,4, 6, 2]
+// let arr =[3, 5,4, 6, 2]
 
-let [a,c, ...rest] =arr ;
+// let [a,c, ...rest] =arr ;
 
-console.log(a,rest);
+// console.log(a,rest);
 
-// Spread
+// // Spread
 
-let arr1 =[3, 5,4, 6, 2]
-let obj1 ={...arr1}
+// let arr1 =[3, 5,4, 6, 2]
+// let obj1 ={...arr1}
 
-console.log(obj1);
+// console.log(obj1);
 
-function sum(v1, v2, v3){
-    return v1 +v2+ v3;
-}
+// function sum(v1, v2, v3){
+//     return v1 +v2+ v3;
+// }
 
-console.log(sum(1,5,8));
-console.log(sum(...arr1));
+// console.log(sum(1,5,8));
+// console.log(sum(...arr1));
 
 
-let obj2 ={
-    name: "pk",
-    company: "happy",
-    address :" XYZ"
+// let obj2 ={
+//     name: "pk",
+//     company: "happy",
+//     address :" XYZ"
 
-}
-console.log({...obj2, name :"hk"})
+// }
+// console.log({...obj2, name :"hk"})
 
-const person = {
-  name: "Priyanshu",
-  age: 20,
-  city: "Ludhiana"
+// const person = {
+//   name: "Priyanshu",
+//   age: 20,
+//   city: "Ludhiana"
+// };
+
+// // normal way
+// const name1 = person.name;
+
+// // destructuring
+// const { name, age, city } = person;
+
+// console.log(name); // Priyanshu
+// console.log(age);  // 20
+
+
+const user = {
+    name: "Priyanshu",
+    address: {
+        city: "Ludhiana",
+        pin: 141001
+    }
 };
 
-// normal way
-const name1 = person.name;
+const { address: { city, pin } } = user;
 
-// destructuring
-const { name, age, city } = person;
-
-console.log(name); // Priyanshu
-console.log(age);  // 20
+console.log(city); // Ludhiana
