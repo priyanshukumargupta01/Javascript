fbdf3a59da705ec7ce64282a9ce7ecff34a603c3
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-17 23:49:09 +0530

    more

--- JS/Function/async.js
+++ JS/Function/async.js
@@ -1,16 +1,27 @@
-function getData() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Data received");
-    }, 2000);
-  });
+// function getData() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Data received");
+//     }, 2000);
+//   });
+// }
+
+
+
+// async function showData() {
+//   let result = await getData();
+//   console.log(result);
+// }
+
+// showData();
+
+async function getData() {
+  try {
+    let res = await Promise.reject("Error occurred");
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-
-async function showData() {
-  let result = await getData();
-  console.log(result);
-}
-
-showData();
+getData();
