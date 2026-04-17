6a9a83d4d9db9775a743a2a25dd0b33c319f1844
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-17 23:51:38 +0530

    more

--- JS/Array/multiDem.js
+++ JS/Array/multiDem.js
@@ -1,20 +1,38 @@
-const NameAndNumber = [["happy", "90"], ["Priyanshu", "99"]];
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
 
 
-console.log(NameAndNumber[0][1]);
+console.log(arr[0][0]); // 1
+console.log(arr[1][2]); // 6
+console.log(arr[2][1]); // 8
 
 
 
-function sumArray(arr) {
-  let sum = 0;
+ 
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      sum += arr[i][j];
-    }
-  }
 
-  return sum;
-}
 
-console.log(sumArray(arr)); 
+
+// const NameAndNumber = [["happy", "90"], ["Priyanshu", "99"]];
+
+
+// console.log(NameAndNumber[0][1]);
+
+
+
+// function sumArray(arr) {
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       sum += arr[i][j];
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(sumArray(arr)); 
