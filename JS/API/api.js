0ca5b587a788ed7f18ba82dd42d3616b4f718210
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-21 23:57:28 +0530

    more

---
+++ JS/API/api.js
@@ -0,0 +1,10 @@
+async function getData() {
+  try {
+    let response = await fetch("https://api.example.com/data");
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
