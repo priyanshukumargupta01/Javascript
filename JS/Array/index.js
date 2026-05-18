5dcafbe824a634c35b9171836a894013d55ffbb5
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-05-18 23:42:55 +0530

    more

--- JS/Array/index.js
+++ JS/Array/index.js
@@ -1,17 +1,18 @@
-/* Array property
+// Array property
 
 let arr = [1, 2, 4, 6] 
 let arr2 = [9, 8, 9, 6, 9, 3]
+
 console.log(arr)
 console.log(arr.length)
 console.log(arr[2])
 console.log(arr[0])
-console.log(arr.toString)
+console.log(arr.toString())
 console.log(arr, typeof arr)
 console.log(arr.join(" and "))
 console.log(arr[2])
-console.log(arr.pop() )
-console.log(arr.push() )
+console.log(arr.pop())
+console.log(arr.push())
 console.log(arr.push("happy"))
 console.log(arr.push("happy"))
 console.log(arr)
@@ -20,7 +21,7 @@ console.log(arr)
 console.log(arr.unshift("ram"))
 console.log(arr)
 console.log(arr.pop())
-console.log( delete arr[2])
+console.log(delete arr[2])
 console.log(arr)
 console.log(arr.concat(arr2))
 console.log(arr)
@@ -31,33 +32,52 @@ console.log(arr.slice(2))
 console.log(arr.reverse())
 
 
-*/
 
+// MAP FILTER
 
-// MAP FIILTER 
-// let arr = [1, 2, 4, 6] ;
-// let newArr =[];
-// for (let index = 0; index < arr.length; index++) {
-//     const element = arr[index]; 
-//     newArr.push(element**2)
-// }
-// console.log(newArr)
+let arr3 = [1, 2, 4, 6];
 
-/*
-let arr = [1, 2, 4, 6] ;
-let newArr = arr.map(e =>{
- return e*e
-}) 
+// Normal loop example
+let newArr = [];
+
+for (let index = 0; index < arr3.length; index++) {
+    const element = arr3[index];
+    newArr.push(element ** 2)
+}
+
 console.log(newArr)
 
-function New(e){
-    if(e>7){
+
+
+// MAP Example
+let squareArr = arr3.map(e => {
+    return e * e
+})
+
+console.log(squareArr)
+
+
+
+// FILTER Example
+function New(e) {
+    if (e > 7) {
         return true
     }
     return false
 }
-console.log(newArr.filter(New))
- */
+
+console.log(squareArr.filter(New))
 
 
 
+
+let marks = [40, 50, 60, 70];
+let updatedMarks = marks.map(m => {
+    return m + 5
+})
+
+console.log(updatedMarks)
+
+
+// Output:
+// [45, 55, 65, 75]
