d27262c2a916afef4273a35243977c8a28598987
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-05-20 23:37:06 +0530

    more

---
+++ JS/revision/condition.js
@@ -0,0 +1,12 @@
+let age = 45;
+
+if(age < 18){
+    console.log("You are a minor.");
+}       
+else if(age >= 18 && age < 65){     
+    console.log("You are an adult.");
+}   
+
+age<= 65 ? console.log("You are a senior citizen.") : console.log("You are not a senior citizen."); 
+
+age >= 18 && age < 65 ? console.log("You are an adult.") : console.log("You are not an adult.");
