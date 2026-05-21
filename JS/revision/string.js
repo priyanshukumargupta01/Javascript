6237c493de53e93f9f85bc298502eee42ad9565a
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-05-21 23:12:27 +0530

    more

---
+++ JS/revision/string.js
@@ -0,0 +1,25 @@
+let name = "Priyanshu"
+
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name[2])
+console.log(name[3])
+console.log(name[5])
+console.log(name[8])
+
+let name1 = "Priyanshu"
+let name2 = "raj"
+
+// console.log(name1 + name2)
+// console.log(name1.concat(name2))
+// console.log("my name is " + name1 + " and my friend name is " + name2)
+// console.log(`my name is ${name1} and my friend name is ${name2}`)
+// console.log(name1.includes("priyanshu"))
+// console.log(name1.includes("Priyanshu"))
+// console.log(name1.startsWith("P"))
+// console.log(name1.endsWith("u"))
+// console.log(name1.indexOf("a"))
+// console.log(name1.indexOf("z"))
+console.log(name1.slice(4,8))
+console.log(name1.slice(4))
