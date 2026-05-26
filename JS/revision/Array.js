efb6d8c0e137008c46600bb4beeeaa72736f3528
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-05-26 23:10:44 +0530

    more

--- JS/revision/Array.js
+++ JS/revision/Array.js
@@ -3,11 +3,9 @@ let arr2 = arr.map(x=>x*2);
 console.log(arr2);
 
 
-
 let nam = ["Ali", "Veli", "Ayşe", "Fatma"];
 let naam2 = nam.map(x=>x.toUpperCase());
 console.log(naam2);
-
 
 
 let name = ["Ali", "Veli", "Ayşe", "Fatma"];
@@ -33,7 +31,7 @@ const names = users.map((user) => {
 console.log(names);
 
 
-
+//map
 let fruits = ["apple", "banana", "cherry"];
 let frut = fruits.map((fruit, index)=>{
     return (index  + " - " + fruit)
@@ -41,4 +39,84 @@ let frut = fruits.map((fruit, index)=>{
 console.log(frut);
 
 
+//foreach
+const fruit = ["Apple", "Banana", "Mango"]; 
+fruit.forEach((fruit, index) => {
+  console.log(index + " - " + fruit);
+});
 
+
+//reduce
+const numbers = [1, 2, 3, 4]; 
+const result = numbers.reduce((acc, curr) => {
+  return acc * curr;
+}, 1); 
+console.log(result);
+
+
+//filter
+const num = [1, 2, 3, 4, 5, 6];
+const evenNumbers = num.filter((n) => n % 2 === 0);
+console.log(evenNumbers);
+
+
+//foreach
+const num = [1, 2, 3, 4, 5, 6];
+num.forEach((n) => {
+  if (n % 2 === 0) {
+    console.log(n);
+  }
+});
+
+
+const names = ["Rahul", "Aman", "Priyanshu", "Ajay"];v
+const result = names.filter((name) => {
+  return name.length > 4;
+});
+console.log(result);
+
+
+const users = [
+  { name: "Rahul", active: true },
+  { name: "Aman", active:false },
+  { name: "Priyanshu", active: true }
+]; 
+const activeUsers = users.filter((user) => {
+  return user.active;
+}); 
+console.log(activeUsers);
+
+
+const users = [
+  { name: "Rahul", age: 17, active: true },
+  { name: "Aman", age: 22, active: false },
+  { name: "Priyanshu", age: 20, active: true },
+  { name: "Rohit", age: 25, active: true }
+];
+let result = users.filter((user)=>{
+  return user.age > 18 && user.active;
+})
+console.log(result);
+
+
+
+const products = [
+  { name: "Laptop", price: 50000 },
+  { name: "Phone", price: 20000 },
+  { name: "Mouse", price: 500 }
+];
+let result = products.map((product)=>{
+  return ("Product: " + product.name + " - " + product.price);
+})
+console.log(result);
+
+
+
+const products = [
+  { name: "Laptop", price: 50000 },
+  { name: "Phone", price: 20000 },
+  { name: "Mouse", price: 500 }
+];
+let result = products.forEach((product)=>{
+  console.log("Product: " + product.name + " - " + product.price);
+});
--- JS/revision/tempCodeRunnerFile.js
+++ JS/revision/tempCodeRunnerFile.js
@@ -1,71 +1,8 @@
-// let g = [1,2,3,4,5];
-
-// for(let i=0; i < g.length; i++){
-//     console.log(g[i])
-// } 
-// for(let i =0; i<=23;i++){
-//     console.log(i)
-// }
-
-
-
-// for(let i = 40; i >= 0; i--){
-//     let ii = i;
-
-
-
-//     if(i == 20){
-//         console.log("Halfway there!")
-//     }else{
-//         console.log("all is well")
-//     }
-//     console.log(ii)
-// }
-
-
-
-// for(let i = 0; i < 30; i++){;
-//     for(j=0; j<30; j++){
-//     console.log(`the value of i is ${i} and the value of j is ${j}`)
-    
-//     }
-// }
-
-
-
-// const fruits = ["Apple", "Banana", "Mango", "Orange",""];
-// for(let i = 0; i< fruits.length; i++){
-//     console.log(`the index of the fruit is ${i} and the name of the fruit is ${fruits[i]}`)
-// }
-
-
-
-
-for(let i = 0; i < 10; i++){
-    if(i % 2 == 0){
-        console.log(`${i} is an even number`)
-    } else if(i === 5){
-        console.log(`5 the become 5`)
-        break
-    }}
-
-
-let i = 1;
-
-do {
-    console.log(i);
-    i++;
-} while (i <= 5);
-
-let i = 1;
-
-while (i <= 5) {
-    console.log(i);
-    i++;
-}
-
-let arr = ["A", "B", "C"];
-
-for (let value of arr) {
-    console.log(value);
-}
+const products = [
+  { name: "Laptop", price: 50000 },
+  { name: "Phone", price: 20000 },
+  { name: "Mouse", price: 500 }
+];
+let result = products.forEach((product)=>{
+  console.log("Product: " + product.name + " - " + product.price);
+});
